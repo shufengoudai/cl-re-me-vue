169f7756b6ae5bd0d6df8c8abaf7364c9be2e0fa
SJNS\ruixin <ruixin@liandisys.com.cn>
2020-09-04 15:31:39 +0800

    首页完成

--- src/views/home/index-test.vue
+++ src/views/home/index-test.vue
@@ -52,9 +52,10 @@
                         <div slot="header" class="clearfix">
                             <span>近期<i class="el-icon-chat-line-square"></i></span>
                         </div>
-                        <div></div><router-link to='login' class="item"><u>1sadsadsadsadsdsadsadsadsadsadsadsadasdsadsa</u></router-link>
-                        <div><router-link to="login" class="item"><u>1dsadsadddddddddddddddddddddddddddd</u></router-link></div>
-                        <div><router-link to="login" class="item"><u>1sdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</u></router-link></div>
+                        <div></div><router-link to='login' class="item"><u>14日内有四位联系人过生日</u></router-link>
+                        <div><router-link to="login" class="item"><u>14日内有两件活动</u></router-link></div>
+                        <div><router-link to="login" class="item"><u>14日内待洽谈的商务活动有2件</u></router-link></div>
+                        <div><router-link to="login" class="item"><u>7日内待登陆的项目报告有2件</u></router-link></div>
                     </el-card>
                 </div>
                 <el-row>
@@ -63,25 +64,28 @@
                             <div slot="header" class="clearfix">
                                 <span>本日计划</span>
                             </div>
-                            <div class="item">1sadsadsadsadsdsadsadsadsadsadsadsadasdsadsa</div>
-                            <div class="item">1dsadsadddddddddddddddddddddddddddd</div>
-                            <div class="item">1sdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+                            <div class="item">10：30拜访联迪信息技术有限公司 联络人：赵 地点：软件谷科创城A1北</div>
+                            <div class="item">12：00联迪信息技术有限公司会议 联络人：赵 地点：联迪食堂</div>
+                            <div class="item">14：00南京市政府会议 联络人：赵 地点：德基二期六楼赤坂亭</div>
+                            <div><router-link to="login" class="item"><u>更多…</u></router-link></div>
                         </el-card>
                         <el-card class="box-card">
                             <div slot="header" class="clearfix">
                                 <span>当前项目状况</span>
                             </div>
-                            <div class="item">1sadsadsadsadsdsadsadsadsadsadsadsadasdsadsa</div>
-                            <div class="item">1dsadsadddddddddddddddddddddddddddd</div>
-                            <div class="item">1sdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+                            <div class="item">现金入帐系统 客户单位：南京银行 联络人：钱多多 进行中 期间：2020/08/01-2020/08/01</div>
+                            <div class="item">远程教育平台 客户单位：东京大学 联络人：木村 进行中 期间：2020/06/01-2020-11-01</div>
+                            <div class="item">母子健康管理APP 客户单位：市妇幼 联络人：赵小小 期间：2020/01/04-2020/09/01</div>
+                            <div><router-link to="login" class="item"><u>更多…</u></router-link></div>
                         </el-card>
                         <el-card class="box-card">
                             <div slot="header" class="clearfix">
                                 <span>近期联系人</span>
                             </div>
-                            <div class="item">1sadsadsadsadsdsadsadsadsadsadsadsadasdsadsa</div>
-                            <div class="item">1dsadsadddddddddddddddddddddddddddd</div>
-                            <div class="item">1sdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+                            <div class="item">张三 长安马自达</div>
+                            <div class="item">钱小 上汽大众</div>
+                            <div class="item">王成 东风本田</div>
+                            <div class="item">李强 华晨宝马</div>
                         </el-card>
                     </el-col>
                     <el-col :span="10">
@@ -89,26 +93,31 @@
                             <div slot="header" class="clearfix">
                                 <span>明天起三日内计划</span>
                             </div>
-                            <div class="item">2</div>
-                            <div class="item">2</div>
-                            <div class="item">2</div>
+                            <div class="item">2020/8/20 9：30拜访长江实业有限公司 联络人：Andy low 地点：中环太平山</div>
+                            <div class="item">2020/8/21 11：00与联迪信息技术有限公司窗口开会 联络人：张三 地点：联迪406</div>
+                            <div class="item">2020/8/22 13：00参加苹果公司产品发布会 联络人：无 地点：未定</div>
+                            <div><router-link to="login" class="item"><u>更多…</u></router-link></div>
                         </el-card>
                         <el-card class="box-card">
                             <div slot="header" class="clearfix">
                                 <span>近期节日</span>
                             </div>
-                            <div class="item">2</div>
-                            <div class="item">2</div>
-                            <div class="item">2</div>
+                            <div class="item">2020/10/1 国庆节</div>
+                            <div class="item">2020/12/25 圣诞节</div>
+                            <div class="item">2021/1/1 元旦</div>
+                            <div class="item">2021/2/1 春节</div>
                             <div><i class="el-icon-date" @click="openCalendar"></i></div>
                         </el-card>
                         <el-card class="box-card">
                             <div slot="header" class="clearfix">
                                 <span>长期未联系</span>
                             </div>
-                            <div class="item">2</div>
-                            <div class="item">2</div>
-                            <div class="item">2</div>
+                            <div class="item">山崎 东京证券</div>
+                            <div class="item">山田 株式会社高岛屋</div>
+                            <div class="item">川本 三越百货</div>
+                            <div class="item">田中 丸井百货</div>
+                            <div class="item">桐生 西日本铁路株式会社</div>
+                            <div><router-link to="login" class="item"><u>更多…</u></router-link></div>
                         </el-card>
                     </el-col>
                 </el-row>
